c6d42726649f95a5e9cfee9a631a3d2a72025ff2
Isaac Cloninger <ijcloninger@gmail.com>
2022-08-22 22:11:31 -0400

    implemented newImage function, started newItem function

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(source, styleLeft, styleBot) {
+    let item = document.createElement('img')
+    item.src = source
+    item.style.position = 'fixed'
+    item.style.left = styleLeft
+    item.style.bottom = styleBot
+    document.body.append(item)
+}
 
-let pineTree = document.createElement('img')
-greenCharacter.src = 'assets/pine-tree.png'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '450px'
-greenCharacter.style.bottom = '200px'
-document.body.append(pineTree)
+function newItem() {
+
+}
+
+// let greenCharacter = document.createElement('img')
+// greenCharacter.src = 'assets/green-character.gif'
+// greenCharacter.style.position = 'fixed'
+// greenCharacter.style.left = '100px'
+// greenCharacter.style.bottom = '100px'
+// document.body.append(greenCharacter)
+
+// let pineTree = document.createElement('img')
+// pineTree.src = 'assets/pine-tree.png'
+// pineTree.style.position = 'fixed'
+// pineTree.style.left = '450px'
+// pineTree.style.bottom = '200px'
+// document.body.append(pineTree)
+
+newImage('assets/green-character.gif', '100px', '100px')
+newImage('assets/pine-tree.png', '450px', '200px')
+newImage('assets/tree.png', '200px', '300px')
+newImage('assets/pillar.png', '350px', '100px')
+newImage('assets/crate.png', '150px', '200px')
+newImage('assets/well.png', '500px', '425px')
+
