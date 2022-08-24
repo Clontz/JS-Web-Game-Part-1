1b799a0a127fc5b8ba192cf3df3a6a3bb3e61b95
Isaac Cloninger <ijcloninger@gmail.com>
2022-08-24 19:52:18 -0400

    added newItem function, invoked new items

--- index.js
+++ index.js
@@ -1,29 +1,41 @@
 function newImage(source, styleLeft, styleBot) {
-    let item = document.createElement('img')
-    item.src = source
-    item.style.position = 'fixed'
-    item.style.left = styleLeft
-    item.style.bottom = styleBot
-    document.body.append(item)
+    let object = document.createElement('img')
+    object.src = source
+    object.style.position = 'fixed'
+    object.style.left = styleLeft
+    object.style.bottom = styleBot
+    document.body.append(object)
+    return object
 }
 
-function newItem() {
+function newItem(source, styleLeft, styleBot) {
+    let object = newImage(source, styleLeft, styleBot)
 
+    object.addEventListener('dblclick', () => {
+        object.remove();
+    })
 }
 
-// let greenCharacter = document.createElement('img')
-// greenCharacter.src = 'assets/green-character.gif'
-// greenCharacter.style.position = 'fixed'
-// greenCharacter.style.left = '100px'
-// greenCharacter.style.bottom = '100px'
-// document.body.append(greenCharacter)
+// // let sword = document.createElement('img')
+// // sword.src = 'assets/sword.png'
+// // sword.position = 'fixed'
+// // sword.left = '500px'
+// // sword.bottom = '405px'
+// // document.body.append(sword)
 
-// let pineTree = document.createElement('img')
-// pineTree.src = 'assets/pine-tree.png'
-// pineTree.style.position = 'fixed'
-// pineTree.style.left = '450px'
-// pineTree.style.bottom = '200px'
-// document.body.append(pineTree)
+// // let greenCharacter = document.createElement('img')
+// // greenCharacter.src = 'assets/green-character.gif'
+// // greenCharacter.style.position = 'fixed'
+// // greenCharacter.style.left = '100px'
+// // greenCharacter.style.bottom = '100px'
+// // document.body.append(greenCharacter)
+
+// // let pineTree = document.createElement('img')
+// // pineTree.src = 'assets/pine-tree.png'
+// // pineTree.style.position = 'fixed'
+// // pineTree.style.left = '450px'
+// // pineTree.style.bottom = '200px'
+// // document.body.append(pineTree)
 
 newImage('assets/green-character.gif', '100px', '100px')
 newImage('assets/pine-tree.png', '450px', '200px')
@@ -31,4 +43,8 @@ newImage('assets/tree.png', '200px', '300px')
 newImage('assets/pillar.png', '350px', '100px')
 newImage('assets/crate.png', '150px', '200px')
 newImage('assets/well.png', '500px', '425px')
+
+newItem('assets/sword.png', '500px', '405px')
+newItem('assets/shield.png', '165px', '185px')
+newItem('assets/staff.png', '600px', '100px')
 
